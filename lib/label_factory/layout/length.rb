2588b8410e3cf1f66ca9ffbadcda7bd55f43120c
gagoar <xeroice@gmail.com>
2013-07-02 12:53:05 -0300

    split in two the xml LengthNode and the Length class itself

--- lib/label_factory/layout/length.rb
+++ lib/label_factory/layout/length.rb
@@ -29,25 +29,5 @@ module LabelFactory
     		end
     	end
     end
-
-    class LengthNode < XML::Mapping::SingleAttributeNode
-
-      def initialize_impl(path)
-        @path = XML::XXPath.new(path)
-      end
-
-      def extract_attr_value(xml)
-        @value = default_when_xpath_err{@path.first(xml).text}
-    		Length.new(@value)
-      end
-
-      def set_attr_value(xml, value)
-        raise "Not a Length: #{value}" unless value.is_a? Length
-        @path.first(xml, ensure_created: true).text = value.value
-      end
-    end
-
-    p 'juanito'
-    XML::Mapping.add_node_class LengthNode
   end
 end
